888b07b1fd2d40927feab01e4f74b9913385e478
Ch1tt0 <jlast07@icloud.com>
2026-06-13 16:08:04 +0200

    Added vscode launch task, added FPS info, added favicon to web.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-use avian3d::prelude::*;
 use bevy::prelude::*;
+
+mod dev_tools;
+use crate::dev_tools::DevToolsPlugin;
+
+mod ui;
+use crate::ui::UIPlugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(DevToolsPlugin)
+        .add_plugins(UIPlugin)
+        .add_systems(Startup, setup)
         .run();
+}
+
+#[derive(Component)]
+struct Player;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
 }
