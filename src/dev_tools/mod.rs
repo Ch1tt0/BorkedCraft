3ec612286e0d5e83edfb68f991a1e84df7931c24
Ch1tt0 <jlast07@icloud.com>
2026-06-18 19:07:16 +0200

    Moved console.rs from ui/ to dev_tools/

--- src/dev_tools/mod.rs
+++ src/dev_tools/mod.rs
@@ -1,5 +1,6 @@
 use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*};
 
+mod console;
 mod performance_info;
 
 pub struct DevToolsPlugin;
@@ -7,6 +8,7 @@ pub struct DevToolsPlugin;
 impl Plugin for DevToolsPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(FrameTimeDiagnosticsPlugin::default())
+            .add_plugins(console::ConsolePlugin)
             .add_plugins(performance_info::PerformanceInfoPlugin);
     }
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,7 +1,5 @@
 use bevy::prelude::*;
 
-mod console;
-
 #[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
 pub enum UIState {
     #[default]
@@ -13,7 +11,6 @@ pub struct UIPlugin;
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<UIState>(); // Initialize UIState
-        app.add_plugins(console::ConsolePlugin);
         app.add_systems(Update, setup);
     }
 }
